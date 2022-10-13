e6aa9ed5eb5bd5e497f314457600b6e10e2b9381
urphilita <urphilita@gmail.com>
2022-10-13 02:31:17 -0400

    Añadir Servicios

---
+++ src/Servicios.java
@@ -0,0 +1,9 @@
+public class Servicios {
+    private String detalle;
+    private String tipo;
+
+    public Servicios(String detalle, String tipo) {
+        this.detalle = detalle;
+        this.tipo = tipo;
+    }
+}
